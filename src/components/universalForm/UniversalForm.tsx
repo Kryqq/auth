3a9315b17a751bf2de2449e54d9b818592cdbcff
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-07-04 23:11:05 +0700

    add defaultValue & types

--- src/components/universalForm/UniversalForm.tsx
+++ src/components/universalForm/UniversalForm.tsx
@@ -1,6 +1,5 @@
-import { Controller, FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import { Controller, FieldValues, Path, PathValue, SubmitHandler, useForm } from 'react-hook-form';
 import { Field } from '../../types/registrationTypes';
-
 
 type UniversalFormProps<T extends FieldValues> = {
    fields: Field<T>[];
@@ -21,12 +20,15 @@ export const UniversalForm = <T extends FieldValues>({ fields, onSubmit }: Unive
                   <label htmlFor={field.name}>{field.label}</label>
                </div>
                <Controller
+                  defaultValue={'' as PathValue<T, Path<T>>}
                   name={field.name}
                   control={control}
                   rules={field.rules || {}}
                   render={({ field }) => <input {...field} />}
                />
-               {errors[field.name] && <div className='errorMessage'>{(errors[field.name]?.message as string) || ''}</div>}
+               {errors[field.name] && (
+                  <div className="errorMessage">{(errors[field.name]?.message as string) || ''}</div>
+               )}
             </div>
          ))}
          <button type="submit">Submit</button>
