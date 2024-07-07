58f5aee990ce2ed35497fd6006552da2b1782935
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-07-07 20:12:16 +0700

    refactor

--- src/layout/registrationForm/RegistrationForm.tsx
+++ src/layout/registrationForm/RegistrationForm.tsx
@@ -1,46 +1,31 @@
-import styles from '../fieldsForm.module.css';
+import React from 'react';
 
-import axios from 'axios';
+import styles from '../fieldsForm.module.css';
 
 import { SubmitHandler } from 'react-hook-form';
 
+import { NavLink, redirect } from 'react-router-dom';
+
 import { UniversalForm } from '../../components/universalForm/UniversalForm';
 
-import { Field, FormData } from '../../types/registrationTypes';
-import { NavLink } from 'react-router-dom';
+import { FormData } from '../../types/registrationTypes';
+
 import { registerUserRequest } from '../../api/requests/requests';
 
-export const RegistrationForm = () => {
-   const onSubmit: SubmitHandler<FormData> = (data) => {
-      registerUserRequest(data);
-   };
+import { fields } from './registrationFields';
 
-   const fields: Field<FormData>[] = [
-      { name: 'username', label: 'Username', rules: { required: 'Username is required' } },
-      {
-         name: 'email',
-         label: 'Email',
-         rules: { required: 'Email is required', pattern: { value: /\S+@\S+\.\S+/, message: 'Invalid email address' } },
-      },
-      {
-         name: 'password',
-         label: 'Password',
-         rules: {
-            min: 3,
-            required: 'Password is required',
-            minLength: { value: 5, message: 'Password is too short' },
-         },
-      },
-      {
-         name: 'PasswordConfirm',
-         label: 'Confirm the password',
-         rules: {
-            min: 3,
-            required: 'Password is required',
-            minLength: { value: 5, message: 'Password is too short' },
-         },
-      },
-   ];
+export const RegistrationForm = () => {
+   const [useRegister, setUseRegister] = React.useState(false);
+   // надо ли тут писать async?
+   const onSubmit: SubmitHandler<FormData> = async (data) => {
+      await registerUserRequest(data, setUseRegister);
+
+      console.log(registerUserRequest);
+
+      if (useRegister) {
+         redirect('/test');
+      }
+   };
 
    return (
       <div className={styles.fieldsForm}>
