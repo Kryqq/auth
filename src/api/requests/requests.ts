048d1c810082a3175fd0cc12c8adde819b72ffe0
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-07-07 20:14:58 +0700

    needs to implemet redirect. now bad decision

--- src/api/requests/requests.ts
+++ src/api/requests/requests.ts
@@ -2,6 +2,8 @@
 import { api } from "../instance";
 import { FormData } from "../../types/registrationTypes";
 import axios from "axios";
+import { redirect } from "react-router-dom";
+import React from "react";
 
 
 const requestConstants = {
@@ -12,15 +14,21 @@ const requestConstants = {
 
 export const registerUser = (data: FormData)=> api.post(requestConstants.REGISTER_USER, data)
 
-export const registerUserRequest = async (data: FormData) => {
+export const registerUserRequest = async (data: FormData, setUseRegister: React.Dispatch<React.SetStateAction<boolean>>) => {
+
+
 	try {
-	    await registerUser(data);
- 	    alert('Registration successful');
+	    await registerUser(data) ;
+	    setUseRegister(true)
+	    alert('Registration successful');
+	
 	} 
 	catch (error) {
 	    if (axios.isAxiosError(error)) {
 		   console.log(error);
+		   
 		   alert(error.response?.data?.message || 'Registration failed');
+
 	    } else {
 		   console.error(error);
 		   alert('An unexpected error occurred');
