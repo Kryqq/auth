89629079cbb37903ad5d308de7dda7122a27af35
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-08-19 22:27:03 +0700

    add paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-	alias: {
-	  '@': path.resolve(__dirname, './src'),
-	  '@api': path.resolve(__dirname, './src/shared/api'),
-	  '@components': path.resolve(__dirname, './src/shared/components'),
-	  '@hooks': path.resolve(__dirname, './src/shared/hooks'),
-	  '@types': path.resolve(__dirname, './src/shared/types'),
-		}
-  	}
-  }
-)
+   plugins: [react()],
+   resolve: {
+      alias: {
+         '@': path.resolve(__dirname, './src'),
+         '@api': path.resolve(__dirname, './src/shared/api'),
+         '@components': path.resolve(__dirname, './src/shared/components'),
+         '@hooks': path.resolve(__dirname, './src/shared/hooks'),
+         '@types': path.resolve(__dirname, './src/shared/types'),
+         '@shared': path.resolve(__dirname, './src/shared'),
+      },
+   },
+});
